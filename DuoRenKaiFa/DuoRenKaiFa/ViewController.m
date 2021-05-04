0c94e2477768aa9432c232a90f352e3c27227ec5
wuwu <1243930473@qq.com>
2021-05-04 15:29:11 +0800

    添加按钮

--- DuoRenKaiFa/DuoRenKaiFa/ViewController.m
+++ DuoRenKaiFa/DuoRenKaiFa/ViewController.m
@@ -15,6 +15,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    UIButton *btn = [[UIButton alloc] init];
+    btn.backgroundColor = [UIColor redColor];
+    [self.view addSubview:btn];
     // Do any additional setup after loading the view.
 }
 
